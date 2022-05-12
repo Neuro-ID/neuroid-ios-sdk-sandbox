09121bf9b3550133247498a3deb2e050035a8381
JosPerez <jose.perez@neuro-id.com>
2022-05-12 09:59:37 -0500

    Create new sandbox
    Add Neuro ID logo to assets
    Create Onboarding Screen
     - Create XIB with the UI
     - Create Controller
    Create Loan Options Screen
     - Create XIB with the UI
     - Create Controller

---
+++ NeuroIdExample/Sandbox/LoanAgree/View/NILoanOptionsVC.swift
@@ -0,0 +1,24 @@
+//
+//  NILoanOptionsVC.swift
+//  NeuroIdExample
+//
+//  Created by jose perez on 12/05/22.
+//
+import UIKit
+final class NILoanOptionsVC: UIViewController {
+    @IBOutlet weak var ageAtWorklbl: UITextField!
+    @IBOutlet weak var economicDependentslbl: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBarImage()
+    }
+    /// Setup navigation bar image with logo
+    private func setupNavBarImage() {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+            imageView.contentMode = .scaleAspectFill
+            let image = UIImage(named: "NeuroID_logo")
+            imageView.image = image
+            navigationItem.titleView = imageView
+    }
+}
