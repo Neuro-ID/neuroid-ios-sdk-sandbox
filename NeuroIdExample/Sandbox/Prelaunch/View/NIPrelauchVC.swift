f91bd0f324566bd098a5b42b11989ff6089a8e01
JosPerez <jose.perez@neuro-id.com>
2022-06-06 11:20:49 -0500

    Update NIPrelauchVC.swift
    Adding function to get the current version from plist

--- NeuroIdExample/Sandbox/Prelaunch/View/NIPrelauchVC.swift
+++ NeuroIdExample/Sandbox/Prelaunch/View/NIPrelauchVC.swift
@@ -10,7 +10,25 @@ final class NIPrelauchVC: UIViewController {
     @IBOutlet weak var versionLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        versionLbl.text = NeuroID.getSDKVersion()
+        versionLbl.text = getCurrentVersion()
+    }
+    ///
+    private func getCurrentVersion() -> String {
+        var config: [String: Any]?
+        if let infoPlistPath = Bundle.main.url(forResource: "Info", withExtension: "plist") {
+            do {
+                let infoPlistData = try Data(contentsOf: infoPlistPath)
+                if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? [String: Any] {
+                    config = dict
+                }
+            } catch {
+                print(error)
+            }
+        }
+        if let version = config?["InternalCurrentVersion"] as? String {
+            return version
+        }
+        return NeuroID.getSDKVersion() ?? "1.0.0"
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
